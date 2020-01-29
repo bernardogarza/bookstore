500aed357803459634b2ef591b0cf4bbabf07b09
Kalib Hackin <nagui08@hotmail.com>
2020-01-29 12:49:03 -0500

    solving linters issues

--- src/components/book.js
+++ src/components/book.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Book = ({id, title, category}) => (
-    <tr>
-        <td>{id}</td>
-        <td>{title}</td>
-        <td>{category}</td>
-    </tr>
+	<tr>
+		<td>{id}</td>
+		<td>{title}</td>
+		<td>{category}</td>
+	</tr>
 )
 
 export default Book;
--- src/components/booksForm.js
+++ src/components/booksForm.js
@@ -3,17 +3,17 @@ import React from 'react';
 const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"];
 
 const BooksForm = () => (
-    <div>
-        <input type='text' placeholder='Title' />
-        <select>
-            {
-                categories.map((category, i) => 
-                    <option value={category} key={i}>{category}</option>
-                )
-            }
-        </select>
-        <button>Add Book</button>
-    </div>
+	<div>
+		<input type='text' placeholder='Title' />
+		<select>
+			{
+				categories.map((category, i) => 
+					<option value={category} key={i}>{category}</option>
+				)
+			}
+		</select>
+		<button>Add Book</button>
+	</div>
 )
 
 export default BooksForm;
