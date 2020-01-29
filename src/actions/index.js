3afa88087296d94494d02fc1f7633cfe0af36a8f
bernardogarza <bernardo.g.landa@gmail.com>
2020-01-29 11:17:25 -0600

    Working with the map method for the books

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+// const ADD_BOOK = 'ADD_BOOK'
+// const REMOVE_BOOK = 'REMOVE_BOOK'
+
+// const mapDispatchToProps = dispatch => {
+//   ADD_BOOK(book) {
+//     dispatch({
+//       type: ADD_BOOK
+//     })
+//   }
+// }
--- src/containers/booksList.js
+++ src/containers/booksList.js
@@ -2,20 +2,24 @@ import React from 'react';
 import Book from '../components/book';
 import { connect } from 'react-redux';
 
-const BooksList = () => (
+const BooksList = books => (
     <table>
         <tr>
             <td>ID</td>
             <td>Title</td>
             <td>Category</td>
         </tr>
-        <Book id='1' title='title 1' category='category 1' />
-        <Book id='2' title='title 2' category='category 2' />
+        <tbody>
+            {console.log(books)}
+            {books.map((book, i) => (<Book id={book.id} title={book.title} category={book.category} key={i} />))}
+        </tbody>
+        {/* <Book id='1' title='title 1' category='category 1' /> */}
+        {/* <Book id='2' title='title 2' category='category 2' /> */}
     </table>
 )
 
 const mapStateToProps = state => ({
-
+    books: state.bookReducer
 })
 
 export default connect(mapStateToProps)(BooksList);
