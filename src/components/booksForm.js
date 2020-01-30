dc6ca14ee39cd7e206191498ebe7804d2b75feb2
Kalib Hackin <nagui08@hotmail.com>
2020-01-30 12:04:26 -0500

    adding books button done

--- src/components/booksForm.js
+++ src/components/booksForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import uniqid from 'uniqid';
 import { connect } from 'react-redux';
+import uniqid from 'uniqid';
+import PropTypes from 'prop-types';
+import { ADD_BOOK } from '../actions';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
@@ -8,7 +10,7 @@ class BooksForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: 1,
+      id: uniqid(),
       title: '',
       category: categories[0],
     };
@@ -16,26 +18,39 @@ class BooksForm extends Component {
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
-    console.log(this.state);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { addBook } = this.props;
+    const { state } = this;
+    if (state.title !== '') {
+      addBook(this.state);
+      this.setState({ id: uniqid() });
+    }
   }
 
   render() {
     return (
-      <div>
+      <form>
         <input type="text" placeholder="Title" name="title" onChange={this.handleChange.bind(this)} />
         <select onChange={this.handleChange.bind(this)} name="category">
           {
-            categories.map(category => <option value={category} >{category}</option>)
+            categories.map(category => <option value={category} key={category}>{category}</option>)
           }
         </select>
-        <button type="submit">Add Book</button>
-      </div>
+        <button type="submit" onClick={this.handleSubmit.bind(this)}>Add Book</button>
+      </form>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
+BooksForm.propTypes = {
+  addBook: PropTypes.func.isRequired,
+};
 
+const mapDispatchToProps = dispatch => ({
+  addBook: book => dispatch(ADD_BOOK(book)),
 });
 
 export default connect(null, mapDispatchToProps)(BooksForm);
