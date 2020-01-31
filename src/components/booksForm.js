b4b6f484d09e23ad4225fbafb98cddf91acfaa23
Kalib Hackin <nagui08@hotmail.com>
2020-01-31 16:26:47 -0500

    category arrow fixed

--- src/components/booksForm.js
+++ src/components/booksForm.js
@@ -38,19 +38,21 @@ class BooksForm extends Component {
         <div className="title-form">ADD NEW BOOK</div>
         <form className="books-form">
           <input id="create-course-form" type="text" placeholder="Book title" name="title" onChange={this.handleChange.bind(this)} />
-          <select onChange={this.handleChange.bind(this)} name="category" className="select-box">
-            {
-              categories.map(category => (
-                <option value={category} key={category}>{category}</option>
-              ))
-            }
-          </select>
-          <span className="icon-select-mate">
+          <div className="styled-select">
+            <select onChange={this.handleChange.bind(this)} name="category" className="select-box">
+              {
+                categories.map(category => (
+                  <option value={category} key={category}>{category}</option>
+                ))
+              }
+            </select>
+          </div>
+          {/* <span className="icon-select-mate">
             <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
               <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z" />
               <path d="M0-.75h24v24H0z" fill="none" />
             </svg>
-          </span>
+          </span> */}
           <button type="submit" onClick={this.handleSubmit.bind(this)}>ADD BOOK</button>
         </form>
       </div>
