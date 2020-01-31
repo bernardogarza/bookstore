e4e3522a8371de7bff5cf221546fb8c9e9a00efc
Kalib Hackin <nagui08@hotmail.com>
2020-01-31 16:33:44 -0500

    I forgot to save a file

--- src/components/booksForm.js
+++ src/components/booksForm.js
@@ -47,12 +47,6 @@ class BooksForm extends Component {
               }
             </select>
           </div>
-          {/* <span className="icon-select-mate">
-            <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z" />
-              <path d="M0-.75h24v24H0z" fill="none" />
-            </svg>
-          </span> */}
           <button type="submit" onClick={this.handleSubmit.bind(this)}>ADD BOOK</button>
         </form>
       </div>
